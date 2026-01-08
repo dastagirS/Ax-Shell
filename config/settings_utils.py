6199b6e655811eb4bee42428206e37b1a5db704c
dastagirS <shroud464@gmail.com>
2026-01-08 18:30:30 +0530

    fix: deprecated layerrule for hyprland
    Signed-off-by: dastagirS <shroud464@gmail.com>

--- config/settings_utils.py
+++ config/settings_utils.py
@@ -282,7 +282,7 @@ bind = {get_bind_var("prefix_restart_inspector")}, {get_bind_var("suffix_restart
 
 source = {home}/.config/{APP_NAME_CAP}/config/hypr/colors.conf
 
-layerrule = noanim, fabric
+layerrulev2 = noanim, fabric
 
 exec = cp $wallpaper ~/.current.wall
 
